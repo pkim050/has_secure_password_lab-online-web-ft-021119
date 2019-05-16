7d1c2742129f8ff7147510f5965d0a08ea66de25
Patrick Kim <pkim050@gmail.com>
2019-05-16 21:52:27 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,19 @@
 class SessionsController < ApplicationController
+  def new
 
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
 end
